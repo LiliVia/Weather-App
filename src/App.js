b3e1f563835b08498683a62a77a14a4cdaeef5f7
Lili Via <lilivenone@gmail.com>
2017-11-30 00:28:02 +0200

    Fetching data for display

--- src/App.js
+++ src/App.js
@@ -6,58 +6,74 @@ const PLACES = [
   { name: 'Berlin' },
   { name: 'Moscow' },
   { name: 'London' },
-  { name: 'Praha' },
+  { name: 'Toronto' },
 ];
 
 class WeatherMap extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null,
+    };
+  }
+
+  componentDidMount() {
+    const name = this.props.name;
+    const URL =
+      'http://api.openweathermap.org/data/2.5/forecast?q=' +
+      name +
+      '&APPID=98040809559bde26b804d64b294bff4c&units=metric';
+
+    fetch(URL)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ weatherData: json });
+      });
+  }
+
   render() {
-    return <h1> Weather map for city {this.props.name}</h1>;
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading...</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = 'http://openweathermap.org/img/w/' + weather.icon + '.png';
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    );
   }
 }
 
 class App extends Component {
-  // state = {
-  //   location: '',
-  //   data: {},
-  // };
-
-  // fetchData = evt => {
-  //   evt.preventDefault();
-
-  //   const location = encodeURIComponent(this.state.location);
-
-  //   const url =
-  //     'http://api.openweathermap.org/data/2.5/forecast?q=' +
-  //     location +
-  //     '&APPID=98040809559bde26b804d64b294bff4c&units=metric';
-
-  //   fetch(url)
-  //     .then(res => res.json())
-  //     .then(json => {
-  //       this.setState({ data: json });
-  //     });
-  // };
-
-  // changeLocation = evt => {
-  //   this.setState({
-  //     location: evt.target.value,
-  //   });
-  // };
+  constructor() {
+    super();
+    this.state = {
+      activeCity: 0,
+    };
+  }
 
   render() {
+    const activeCity = this.state.activeCity;
     return (
       <div className="App">
         {PLACES.map((place, index) => (
           <button
             key={index}
             onClick={() => {
-              console.log('clicked index ' + index);
+              this.setState({ activeCity: index });
             }}
           >
             {place.name}
           </button>
         ))}
-        <WeatherMap name={'Kiev'} />
+        <WeatherMap key={activeCity} name={PLACES[activeCity].name} />
       </div>
     );
   }
